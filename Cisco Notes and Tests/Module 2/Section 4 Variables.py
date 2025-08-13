46e8b2e6468def10fb11ca54f472c54e1d505ef3
CycleOfLearning <herewego1938@gmail.com>
2025-08-13 19:20:23 -0400

    additional explaination of variable in my own words + examples.

--- Cisco Notes and Tests/Module 2/Section 4 Variables.py
+++ Cisco Notes and Tests/Module 2/Section 4 Variables.py
@@ -1,5 +1,11 @@
 ## Variables!!! :DDDDDDDDDD, I used probably like one variable in section 3
-# some variables include
+# some variables include, variables are also kinda like storing it in a box, which then you would use later
+# you'd pull it out using a print command
 
 gotcha = 2 + 2
 print(gotcha)
+
+roundednumber5 = 500 / 25 # This stores value into rounded number 5, that value is the result of 500/25
+print(roundednumber5)
+
+print(roundednumber5 + gotcha) # should print 24. if it doesn't then uhhh something wrong.
