a10d8ca6cb7a708ef65c09f1fa0d1127de289642
CycleOfLearning <herewego1938@gmail.com>
2025-08-13 20:06:47 -0400

    variables more ya

--- Cisco Notes and Tests/Module 2/Section 4 Variables.py
+++ Cisco Notes and Tests/Module 2/Section 4 Variables.py
@@ -9,3 +9,28 @@ roundednumber5 = 500 / 25 # This stores value into rounded number 5, that value 
 print(roundednumber5)
 
 print(roundednumber5 + gotcha) # should print 24. if it doesn't then uhhh something wrong.
+
+# Variables consist of two parts -> value & name
+# roundednumber5 is one variable, other such variables in other languages work as well,
+# no spaces allowed, and must begin with a letter, symbols, numbers will not work.
+
+inches3rd = 2+3
+# 3inches = 2+3 ## THIS will show up as invalid.
+
+print(inches3rd)
+
+## There's keywords such as False, True, elif, def, etc. Try not to use them for names/variables.
+# it probably would show up as invalid. OR cannot be assigned.
+
+# False = 2 + 2 -> INVALID
+myFalse = 300/60
+print(myFalse)
+
+## Some variable declarations below. BTW, MAKE sure to always be accurate w/ variable names, or error/invalid will appear.
+# ex -> if instanceguh was printed w/ capital I, console will print a nameError and redirect to a similar name.
+instanceguh = 250.0
+instanceguh = instanceguh / 50
+accbal = 78
+accname = "DashedTony"
+print(instanceguh, accbal, accname)
+print("The current balance of", accname, "is:", instanceguh-accbal,)
