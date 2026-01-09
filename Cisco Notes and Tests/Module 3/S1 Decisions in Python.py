10f538c7ed8f3824d8509d0d7ca9d3bb25bdf6a7
CycleOfLearning <herewego1938@gmail.com>
2026-01-08 21:15:02 -0500

    oh hey, progress, in one section mostly yeah.

--- Cisco Notes and Tests/Module 3/S1 Decisions in Python.py
+++ Cisco Notes and Tests/Module 3/S1 Decisions in Python.py
@@ -1,4 +1,5 @@
 # It's that time again. Back on it. 1/5/26 Actually doing it this time.
+from selectors import SelectSelector
 
 # depending on the code written -> always a true/false answer. no inbetween like unsure/letmethink. but this techniqually depends on operators.
 
@@ -80,10 +81,99 @@ print("mph value is:", mph_1st <= mph_example) # if number = same then still tru
 
 # Use comparison operators provided and n as input and print true/false for output.
 
-n = int(input("Please input a number!"))
-print("The number of this n is:", n)
+# n = int(input("Please input a number!"))
+# print("The number of this n is:", n)
 
-onehundred = 100
-testn = int(input("What number would you like to input?"))
-print("The inputted number is:", testn, "then it is", testn >= onehundred)
+#onehundred = 100
+#testn = int(input("What number would you like to input?"))
+#print("The inputted number is:", testn, "then it is", testn >= onehundred)
 
+### Conditions/Conditional execution.
+
+# oh boy if then statements?? :DDD
+
+# Special instruction -> conditional instruction: what does it do?
+    # if true_or_not -> if true then go here, if not, go to a different pathway.
+    # basically python is now given an option to do something if that something matches the criteria of true.
+
+## test -> sheep >= 170
+
+sheeptotal_counter = 165
+if sheeptotal_counter >= 170:
+    print("The current total of sheep on the barn is", sheeptotal_counter) # since the sheep total < 170, no print shall happen.
+print("bah bah im a sheep")
+# also note, the if function is a whole block, if i were to say another function out side of it then it'll still run.
+
+bread_counter = 46
+if bread_counter >= 50:
+    print("wow that's alot of bread")
+else:
+    print("mmm, maybe you need more to donate?")
+    print("get more bread bro.")
+# if -> thing = or >= integer then -> print(1st statement), else? -> print(2nd statement).
+# in short -> if criteria is not met then it does something else.
+# also remember -> colons, both the if statement and else statement needs colons. think of the if + else they both of blocks
+# as well as their own branches, so i can put more print statemetns in them.
+
+#nested if-else statements; my own wacky example.
+# if trex_are_alive:
+#   if weapons_are_around:
+#       defend() # call function -> do this
+#    else:
+        #hide() # if does not match then do this
+#else:
+#    if flyingsharks_are_real: #now if trex isnt real then it'll move to the else function.
+        #getinanddrive() # do all these functions if matched. also where did you get a car?
+        #acclerate()
+        #leavepremises()
+#    else:
+        #wakeup_dream()
+
+## Cascading -> if-elif-else
+# basically and order to check and if it doesnt work it'll go down until it works or to the last else statement.
+chicken5 = 0
+
+if chicken5 >= 5:
+    print("hooray for 5 chickens")
+elif chicken5 == 4:
+    print(chicken5, "christmas trees")
+elif chicken5 == 0: # okay figure out how to do this one later? make it so like its a certain range from 1 to 4.
+    print("oh wow nothing")
+
+# side note -> i could mnake it a bit more tighter w/o the use of indents liek this
+if chicken5 >= 5: print("hooray for 5 chickens huzzah")
+elif chicken5 == 4: print(chicken5, "christmas trees, christmass")
+elif chicken5 == 0: print("oh wow nothing, fr? ")
+
+# me personally i like my indents, so i'll probably not do this.
+
+### the looping methods :)
+
+# psudo code -> readable but unable to be executed, also thing about loops
+# you set the function to read that specific integer -> exit(), so it'll keep looping over and over until it gets the right
+# number.
+
+### LAB -> Comparison Operators and conditional execution
+# Spathiphyllum -> must match this exactly, if lowercase -> needs capital s, if something unrelated -> then say no.
+
+# this was supposed to be an input but b/c idk how to break or end it b4 input like after 10 seconds, i set it as a stored variable.
+thatdamnplantname = "Spathiphyllum"
+if thatdamnplantname == "Spathiphyllum":
+    print("Hooray! Spathipyllum is the best plant")
+elif thatdamnplantname == "spathiphyllum":
+    print("did you forget to capitalize Spathiphyllum?")
+else: #oh and btw, you could do elif thatdamnplantname != "spath" or "Spath" and i think it still works, but else is fine.
+    print("remember: Spathiphyllum not,", thatdamnplantname)
+
+### LAB -> Essentials of if-else statement
+# Writing a tax calculator, try to actually break this into chunks lol?
+# This will be using cisco's given data, but adding on for the if-else function.
+
+income = float(input("Enter the annual income: "))
+
+if income < 85528:
+    tax = income * 0.18 - 556.02
+
+tax = round(tax, 0) # if somehow income is above 855528 it'll not be stored in tax. the < operator takes it from
+# 85528 to negative infinity.
+print("The tax is:", tax, "thalers")
